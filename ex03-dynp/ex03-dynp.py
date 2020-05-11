efef5ef157f32e4dbe682593ca3013bf9277b344
Gregor Müller <gregor.mueller94@gmail.com>
2020-05-11 22:01:11 +0200

    First attemp of solving task. Errors in implemented function

--- ex03-dynp/ex03-dynp.py
+++ ex03-dynp/ex03-dynp.py
@@ -22,8 +22,36 @@ def value_iteration():
     gamma = 0.8
     # TODO: implement the value iteration algorithm and return the policy
     # Hint: env.P[state][action] gives you tuples (p, n_state, r, is_terminal), which tell you the probability p that you end up in the next state n_state and receive reward r
-
-
+    delta = 0
+    
+    while True:
+        
+        for s in range(n_states):
+            v = V_states[s]
+            V_a = np.zeros(n_actions) 
+            for a in range(n_actions):
+                transition_matrix = np.array(env.P[s][a])
+                p_vector = transition_matrix[:,0]
+                nstate_vector = transition_matrix[:,1]
+                r_vector = transition_matrix[:,2]
+                
+                V_a[a] = 0
+                for i in range(0,3):
+                    index = nstate_vector[i]
+                    V_a[a]+=p_vector[i] * (r_vector[i]) # + gamma*V_states[index]) 
+                    # TODO: FIx that shit
+                                                               
+            a_max = np.argmax(V_a)
+            V_states[s] = V_a[a_max]
+            
+            delta = np.max(delta,np.abs(v - V_states[s]))
+    
+        if (delta < theta):
+            policy = np.zeros(n_states)
+            for s in range(n_states):
+                policy[s] = np.argmax(V_states)
+    
+    return policy
 def main():
     # print the environment
     print("current environment: ")
