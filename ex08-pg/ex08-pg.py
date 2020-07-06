14b0a83da1cd90fba280d787b465cafcf6f968dd
Gregor Müller <gregor.mueller94@gmail.com>
2020-07-06 12:17:19 +0200

    Working version of REINFORCE

--- ex08-pg/ex08-pg.py
+++ ex08-pg/ex08-pg.py
@@ -5,12 +5,15 @@ import matplotlib.pyplot as plt
 
 def policy(state, theta):
     """ TODO: return probabilities for actions under softmax action selection """
-    h = state @ theta
-    pi_1 = np.exp(h[0]) / (np.exp(h[0]) + np.exp(h[1]))
-    pi_2 = np.exp(h[1]) / (np.exp(h[0]) + np.exp(h[1]))
-    return np.array([pi_1,pi_2])
-    #return [0.5, 0.5]  # both actions with 0.5 probability => random
+    z = state.dot(theta)
+    exp = np.exp(z)
+    return exp/np.sum(exp)
 
+
+
+def softmax_grad(softmax):
+    s = softmax.reshape(-1,1)
+    return np.diagflat(s) - np.dot(s, s.T)
 
 def generate_episode(env, theta, display=False):
     """ enerates one episode and returns the list of states, the list of rewards and the list of actions of that episode """
@@ -23,10 +26,7 @@ def generate_episode(env, theta, display=False):
             env.render()
             
         p = policy(state, theta)
-        try:
-            action = np.random.choice(len(p), p=p)
-        except:
-            print("ALARM! ALARM!!")
+        action = np.random.choice(len(p), p=p)
         state, reward, done, info = env.step(action)
         rewards.append(reward)
         actions.append(action)
@@ -39,13 +39,13 @@ def generate_episode(env, theta, display=False):
 
 def REINFORCE(env):
     theta = np.random.rand(4, 2)  # policy parameters
-    alpha = 0.3
-    gamma = 0.8
+    alpha = 0.0025
+    gamma = 0.99
     last_100_episodes = []
-
-    for e in range(1000):
-        if e % 1000 == 0:
-            states, rewards, actions = generate_episode(env, theta, False)  # display the policy every 300 episodes
+    n_episodes = 1200
+    for e in range(n_episodes):
+        if e % 100 == 0:
+            states, rewards, actions = generate_episode(env, theta, True)  # display the policy every 300 episodes
         else:
             states, rewards, actions = generate_episode(env, theta, False)
 
@@ -57,6 +57,8 @@ def REINFORCE(env):
             last_100_episodes.append(sum(rewards))
             last_100_episodes.pop(0)
         mean = np.mean(last_100_episodes)
+        #if e % 200 == 0:
+           # alpha = alpha/10
         if e % 100 == 0:
             print("episode: " + str(e) + " length: " + str(len(states)) + " Mean of last 100 episodes: " + str(mean)) 
         steps = len(states)
@@ -69,7 +71,10 @@ def REINFORCE(env):
                 G_t[t] += np.power(gamma,k-t-1) * rewards[k-1]
             pi = policy(states[t], theta)
             # update only theta of taken action A_t
-            theta = theta + alpha * np.power(gamma, t) * G_t[t] * (states[t].reshape(4,1) @ (1 - pi).reshape(1,2))
+            action = actions[t]
+            dpi = softmax_grad(pi)[action,:]
+            dlog = dpi[action] / pi[action]
+            theta[:,action] = theta[:,action] + alpha * np.power(gamma, t) * G_t[t] * (states[t] * (1 - pi[action]))
             if np.isnan(theta).any():
                 print("ALARM! ALAAAARM!!")
         debug = True
